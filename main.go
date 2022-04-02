b04977f43bfd0248a686f1183be814ea6bff4630
Andrew-1000 <andruambia95@gmail.com>
2022-04-02 22:11:58 +0300

    Setting up routes, initializing database and running the main function

--- main.go
+++ main.go
@@ -1,5 +1,51 @@
 package main
 
 import (
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+	"hamakeja_api/database"
+	"github.com/jinzhu/gorm"
+	"hamakeja_api/models"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 	
 )
+
+
+func setupRoutes(app *fiber.App) {
+
+	api := app.Group("/api", logger.New())
+
+	api.Post("/truck", models.CreateTruck)
+	api.Get("/trucks", models.GetTrucks)
+	api.Get("/truck/:id", models.GetTruck)
+	api.Patch("/truck/:id", models.UpdateTruck)
+	api.Delete("/truck/:id", models.DeleteTruck)
+
+}
+
+func initDatabase() {
+	var err error
+	database.DBConn, err = gorm.Open("postgres", "host=localhost port=5432 user=andru dbname=hamakeja_api password=1234")
+
+	if err != nil {
+		panic("Failed to connect to database")
+	}
+
+	fmt.Println("Connection opened to db")
+
+	database.DBConn.AutoMigrate(&models.Truck{})
+	fmt.Println("Database Migrated")
+}
+
+func main() {
+	app := fiber.New()
+
+	initDatabase()
+
+	setupRoutes(app)
+
+	app.Listen(":3000")
+
+	defer database.DBConn.Close()
+}
